e018a9e06d5f74fb5dd44d2c70bab985a6bc75da
Jan Willem Stegink <90254539+janwillemstegink@users.noreply.github.com>
2022-01-31 22:06:18 +0100

    Add files via upload

--- whois_output.php
+++ whois_output.php
@@ -58,7 +58,7 @@ foreach ($xml->xpath('//domain') as $item)	{
 	$html_text .= '<tr><td>registrar_whois_server</td><td>'.$item->registrar->registrar_whois_server.'</td></tr>';
 	$html_text .= '<tr><td>registrar_url</td><td>'.$item->registrar->registrar_url.'</td></tr>';
 	$html_text .= '<tr><td>registrar_iana_id</td><td>'.$item->registrar->registrar_iana_id.'</td></tr>';
-	$html_text .= '<tr><td>registrar_held_back</td><td>'.$item->registrar->registrar_held_back.'</td></tr>';
+	$html_text .= '<tr><td>registrar_hidden</td><td>'.$item->registrar->registrar_hidden.'</td></tr>';
 	$html_text .= '<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr>';
 	$html_text .= '<tr><td>reseller_contact_id</td><td>'.$item->reseller->reseller_contact_id.'</td></tr>';
 	$html_text .= '<tr><td>reseller_name</td><td>'.$item->reseller->reseller_name.'</td></tr>';
@@ -69,7 +69,7 @@ foreach ($xml->xpath('//domain') as $item)	{
 	$html_text .= '<tr><td>reseller_country_code</td><td>'.$item->reseller->reseller_country_code.'</td></tr>';
 	$html_text .= '<tr><td>reseller_country_name</td><td>'.$item->reseller->reseller_country_name.'</td></tr>';
 	$html_text .= '<tr><td>reseller_country_language</td><td>'.$item->reseller->reseller_country_language.'</td></tr>';
-	$html_text .= '<tr><td>reseller_held_back</td><td>'.$item->reseller->reseller_held_back.'</td></tr>';
+	$html_text .= '<tr><td>reseller_hidden</td><td>'.$item->reseller->reseller_hidden.'</td></tr>';
 	$html_text .= '<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr>';
 	$html_text .= '<tr><td>registrant_holder_contact_id</td><td>'.$item->registrant->registrant_holder_contact_id.'</td></tr>';
 	$html_text .= '<tr><td><b>registrant_holder_name</b></td><td><b>'.$item->registrant->registrant_holder_name.'</b></td>
@@ -86,35 +86,35 @@ foreach ($xml->xpath('//domain') as $item)	{
 	$html_text .= '<tr><td>registrant_holder_country_code</td><td>'.$item->registrant->registrant_holder_country_code.'</td></tr>';
 	$html_text .= '<tr><td>registrant_holder_country_name</td><td>'.$item->registrant->registrant_holder_country_name.'</td></tr>';
 	$html_text .= '<tr><td>registrant_holder_country_language</td><td>'.$item->registrant->registrant_holder_country_language.'</td></tr>';
-	$html_text .= '<tr><td>registrant_holder_held_back</td><td>'.$item->registrant->registrant_holder_held_back.'</td></tr>';
+	$html_text .= '<tr><td>registrant_holder_hidden</td><td>'.$item->registrant->registrant_holder_hidden.'</td></tr>';
 	$html_text .= '<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr>';
 	$html_text .= '<tr><td>admin_contact_id</td><td>'.$item->admin->admin_contact_id.'</td></tr>';
 	$html_text .= '<tr><td>admin_email</td><td>'.$item->admin->admin_email.'</td>
 	<td>Admin beantwoordt en adresseert een gemeld probleem voor een oplossing.</td>
 	<td>Admin answers and addresses a reported issue for a solution.</td></tr>';
-	$html_text .= '<tr><td>admin_held_back</td><td>'.$item->admin->admin_held_back.'</td></tr>';
+	$html_text .= '<tr><td>admin_hidden</td><td>'.$item->admin->admin_hidden.'</td></tr>';
 	$html_text .= '<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr>';
 	if (strlen(trim($item->tech->contact_1->tech_email_1)))	{	
 		$html_text .= '<tr><td>tech_contact_id_1</td><td>'.$item->tech->contact_1->tech_contact_id_1.'</td></tr>';
 		$html_text .= '<tr><td>tech_email_1</td><td>'.$item->tech->contact_1->tech_email_1.'</td>
 		<td>Eén van de technische contacten reageert om een storingsmelding op te lossen.</td>
 		<td>One of the technical contacts responds to resolve a malfunction notification.</td></tr>';
-		$html_text .= '<tr><td>tech_held_back_1</td><td>'.$item->tech->contact_1->tech_held_back_1.'</td></tr>';
+		$html_text .= '<tr><td>tech_hidden_1</td><td>'.$item->tech->contact_1->tech_hidden_1.'</td></tr>';
 	}
 	if (strlen(trim($item->tech->contact_2->tech_email_2)))	{	
 		$html_text .= '<tr><td>tech_contact_id_2</td><td>'.$item->tech->contact_2->tech_contact_id_2.'</td></tr>';
 		$html_text .= '<tr><td>tech_email_2</td><td>'.$item->tech->contact_2->tech_email_2.'</td></tr>';
-		$html_text .= '<tr><td>tech_held_back_2</td><td>'.$item->tech->contact_2->tech_held_back_2.'</td></tr>';
+		$html_text .= '<tr><td>tech_hidden_2</td><td>'.$item->tech->contact_2->tech_hidden_2.'</td></tr>';
 	}
 	if (strlen(trim($item->tech->contact_3->tech_email_3)))	{	
 		$html_text .= '<tr><td>tech_contact_id_3</td><td>'.$item->tech->contact_3->tech_contact_id_3.'</td></tr>';
 		$html_text .= '<tr><td>tech_email_3</td><td>'.$item->tech->contact_3->tech_email_3.'</td></tr>';
-		$html_text .= '<tr><td>tech_held_back_3</td><td>'.$item->tech->contact_3->tech_held_back_3.'</td></tr>';
+		$html_text .= '<tr><td>tech_hidden_3</td><td>'.$item->tech->contact_3->tech_hidden_3.'</td></tr>';
 	}
 	if (strlen(trim($item->tech->contact_4->tech_email_4)))	{	
 		$html_text .= '<tr><td>tech_contact_id_4</td><td>'.$item->tech->contact_4->tech_contact_id_4.'</td></tr>';
 		$html_text .= '<tr><td>tech_email_4</td><td>'.$item->tech->contact_4->tech_email_4.'</td></tr>';
-		$html_text .= '<tr><td>tech_held_back_4</td><td>'.$item->tech->contact_4->tech_held_back_4.'</td></tr>';
+		$html_text .= '<tr><td>tech_hidden_4</td><td>'.$item->tech->contact_4->tech_hidden_4.'</td></tr>';
 	}	
 	$html_text .= '<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr>';
 	if (strlen(trim($item->name_servers->server_1->server_name_1)))	{
