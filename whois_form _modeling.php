3ad9c291bf3b5073e0ba8848dac06b9ab31b8d13
Jan Willem Stegink <90254539+janwillemstegink@users.noreply.github.com>
2022-02-15 20:55:19 +0100

    Add files via upload

--- whois_form _modeling.php
+++ whois_form _modeling.php
@@ -105,8 +105,8 @@ foreach ($xml->xpath('//domain') as $item)	{
 	<td>Als "web_publish" is ingesteld op ja, kunnen zoekmachines legaal publiceren.</td>
 	<td>Having "web_publish" set to yes, search engines can publish legally.</td></tr>';
 	$html_text .= '<tr><td><b>domain_business_use</b></td><td><b>'.$item->domain_business_use.'</b></td>
-	<td>Vereiste bedrijfsgegevens gebaseerd op het "business_use" (form-)veld.</td>
-	<td>Required company information based on the "business use" (form) field.</td></tr>';
+	<td>Voor klantenbescherming en beheersbare naamgeving: "business_use" ja/nee.</td>
+	<td>For customer protection and manageable naming: "business_use" yes/no.</td></tr>';
 	$html_text .= '<tr><td>domain_status</td><td>'.$item->domain_status.'</td></tr>';
 	$html_text .= '<tr><td>domain_creation</td><td>'.$item->domain_creation.'</td></tr>';
 	$html_text .= '<tr><td>domain_last_renewal</td><td>'.$item->domain_last_renewal.'</td></tr>';
