f502403328b95b328f26ef59b3f5b63435ef8b6d
Jan Willem Stegink <techdesk@hostfusion.nl>
2025-02-12 14:50:49 +0100

    Add files via upload

--- modeling_domain.php
+++ modeling_domain.php
@@ -218,8 +218,8 @@ function SwitchTranslation(translation)	{
 		document.getElementById("details_extensions_values").textContent = "'Eligibility': Hoe een domein voldoet aan een specifieke vereiste in een topleveldomeinzone.";
 		document.getElementById("registrant_role").textContent = "De domeingebruiker die de daadwerkelijke of effectieve controle heeft voor domeinrecht in het land van vestiging.";
 		document.getElementById("registrant_web_id").textContent = proposed + "Webidentificatienummer voor bedrijfsentiteiten en natuurlijke personen.";
-		document.getElementById("registrant_organization").textContent = "De naam van een organisatie die primair verantwoordelijk is voor het domeinabonnement.";
-		document.getElementById("registrant_full_name").textContent = "De naam van een persoon of rol die primair verantwoordelijk is voor het domeinabonnement.";
+		document.getElementById("registrant_organization").textContent = "De organisatie die primair verantwoordelijk is voor het domeinabonnement als registrant.";
+		document.getElementById("registrant_full_name").textContent = "De persoon of rol die als registrant primair verantwoordelijk is voor het domeinabonnement.";
 		document.getElementById("registrant_kind").textContent = "Leeg / 'org' / 'individual' (Voor continuïteit: levenstestament + testament + digitale executeur)";
 		document.getElementById("registrant_name").textContent = "In het RDAP-protocol kan een persoonlijke naam openbaar zichtbaar zijn in het veld 'full_name', zie cira.ca.";
 		document.getElementById("registrant_country_code").textContent = "De ISO-2-landcode-indexering werkt, bijvoorbeeld voor het Verenigd Koninkrijk, dat de EU heeft verlaten.";
@@ -287,8 +287,8 @@ function SwitchTranslation(translation)	{
 		document.getElementById("details_extensions_values").textContent = "'Eligibility': How a domain fulfills a specific requirement in a top-level domain zone.";
 		document.getElementById("registrant_role").textContent = "The domain user who has the actual or effective control for domain rights in the country of establishment.";
 		document.getElementById("registrant_web_id").textContent = proposed + "Web Identification number for business entities and natural persons.";
-		document.getElementById("registrant_organization").textContent = "The name of an organization primarily responsible for the domain subscription.";
-		document.getElementById("registrant_full_name").textContent = "The name of a person or role primarily responsible for the domain subscription.";
+		document.getElementById("registrant_organization").textContent = "The organization primarily responsible for the domain subscription as the registrant.";
+		document.getElementById("registrant_full_name").textContent = "The person or role who, as registrant, is primarily responsible for the domain subscription.";
 		document.getElementById("registrant_kind").textContent = "Empty / 'org' / 'individual' (For continuity: Living Will + Will + Digital Executor)";
 		document.getElementById("registrant_name").textContent = "In the RDAP protocol, a personal name can be publicly visible in the 'full_name' field, see cira.ca.";
 		document.getElementById("registrant_country_code").textContent = "ISO-2 country code indexing works, as for the United Kingdom, which has left the EU.";
@@ -356,8 +356,8 @@ function SwitchTranslation(translation)	{
 		document.getElementById("details_extensions_values").textContent = "'Eligibility': Wie eine Domäne eine bestimmte Anforderung in einer Top-Level-Domänenzone erfüllt.";
 		document.getElementById("registrant_role").textContent = "Der Domänenbenutzer, der die tatsächliche oder effektive Kontrolle hat für Domainrechte im Wohnsitzland.";
 		document.getElementById("registrant_web_id").textContent = proposed + "Web-Identifikationsnummer für Unternehmen und natürliche Personen.";
-		document.getElementById("registrant_organization").textContent = "Der Name einer Organisation, die hauptsächlich für das Domänenabonnement verantwortlich ist.";
-		document.getElementById("registrant_full_name").textContent = "Der Name einer Person oder Rolle, die hauptsächlich für das Domänenabonnement verantwortlich ist.";
+		document.getElementById("registrant_organization").textContent = "Die Organisation, die als Registrant hauptsächlich für das Domänenabonnement verantwortlich ist.";
+		document.getElementById("registrant_full_name").textContent = "Die Person oder Rolle, die als Registrant hauptsächlich für das Domänenabonnement verantwortlich ist.";
 		document.getElementById("registrant_kind").textContent = "Leer / 'org' / 'individual' (Für Kontinuität: Patientenverfügung + Testament + digitaler Testamentsvollstrecker)";
 		document.getElementById("registrant_name").textContent = "Im RDAP-Protokoll kann ein Personenname im Feld 'full_name' öffentlich sichtbar sein, siehe cira.ca.";
 		document.getElementById("registrant_country_code").textContent = "Die Indizierung mit dem ISO-2-Ländercode funktioniert, wie für das Vereinigte Königreich, das die EU verlassen hat.";
@@ -425,8 +425,8 @@ function SwitchTranslation(translation)	{
 		document.getElementById("details_extensions_values").textContent = "'Eligibility' : comment un domaine répond à une exigence spécifique dans une zone de domaine de premier niveau.";
 		document.getElementById("registrant_role").textContent = "L'utilisateur du domaine qui a le contrôle réel ou effectif pour les droits de domaine dans le pays de résidence.";
 		document.getElementById("registrant_web_id").textContent = proposed + "Numéro d’identification Web pour les entités commerciales et les personnes physiques.";
-		document.getElementById("registrant_organization").textContent = "Le nom d’une organisation principalement responsable de l’abonnement au domaine.";
-		document.getElementById("registrant_full_name").textContent = "Le nom d'une personne ou d'un rôle principalement responsable de l'abonnement au domaine.";
+		document.getElementById("registrant_organization").textContent = "L'organisation principalement responsable de l'abonnement au domaine en tant que titulaire.";
+		document.getElementById("registrant_full_name").textContent = "La personne ou le rôle qui, en tant que titulaire, est principalement responsable de l'abonnement au domaine.";
 		document.getElementById("registrant_kind").textContent = "Vide / 'org' / 'individual' (Pour la continuité : testament biologique + testament + exécuteur testamentaire numérique)";
 		document.getElementById("registrant_name").textContent = "Dans le protocole RDAP, un nom personnel peut être visible publiquement dans le champ 'full_name', voir cira.ca.";
 		document.getElementById("registrant_country_code").textContent = "L'indexation des codes pays ISO-2 fonctionne, comme pour le Royaume-Uni, qui a quitté l'UE.";
